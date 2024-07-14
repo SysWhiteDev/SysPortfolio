05c6dc11b2b3aa2ae3602924bda4381f84805e36
SysWhiteDev <syswhite.commercial@gmail.com>
2024-07-14 22:12:12 +0200

    Updated opengraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,14 @@ export const metadata: Metadata = {
     siteName: "SysWhite's Portfolio",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "https://syswhite.dev/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "SysWhite's Portfolio",
+      },
+    ],
   },
 };
 
